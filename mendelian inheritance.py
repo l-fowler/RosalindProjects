f2f33eabafa0da89e329213379a9e11b786ad09c
l-fowler <77890312+l-fowler@users.noreply.github.com>
2021-01-23 17:24:47 +0000

    Add files via upload

---
+++ mendelian inheritance.py
@@ -0,0 +1,31 @@
+
+# Given: Three positive integers k, m, and n, representing a population containing
+# k+m+n organisms: k individuals are homozygous dominant for a factor, m are
+# heterozygous, and n are homozygous recessive.
+
+# Return: The probability that two randomly selected mating organisms will produce
+# an individual possessing a dominant allele (and thus displaying the dominant
+# phenotype). Assume that any two organisms can mate.
+
+#mendel category to probability
+x = input("Enter first genotype (AA, Aa, aa): ")
+y = input("Enter second genotype (AA, Aa, aa): ")
+
+A=1/4
+a=1/4
+AA = A + A, red
+aa = a + a, white
+Aa = A + a, red
+print(AA)
+print(Aa)
+print(aa)
+
+#if x + y:
+ #   AA
+  #  print("100% of offspring are homogenous dominant (AA), red phenotype")
+
+if "AA" + "Aa":
+    print("")
+
+
+

4945b48f1de67afb26ca16ad2e080e0c5db4affe
l-fowler <77890312+l-fowler@users.noreply.github.com>
2021-01-23 16:57:11 +0000

    Add files via upload

---
+++ counting point mutations.py
@@ -0,0 +1,24 @@
+
+#input 2 sample datasets
+s = input("Enter first sample dataset: ")
+t = input("Enter second sample dataset: ")
+
+#length of string cannot exceed 1000 bp length
+if (len(s) or len(t)) >1000:
+    print("DNA strings exceed 1000bp length")
+
+#lengths of strings must match
+elif len(s) != len(t):
+    print("length of strings do not match in length")
+
+#compare characters at index for both strings / the Hamming distance dH(s,t)
+else:
+    count = 0
+    for i in range(len(s)):
+        if t[i] != s[i]:
+            count +=1
+    print(count)
+
+#GAGCCTACTAACGGGAT
+#CATCGTAATGACGGCCT
+#7
